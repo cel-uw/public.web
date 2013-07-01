383ca296534fcd1a020bd01ee59d328349abd9f6
Lucian Dipeso <ldipeso@gmail.com>
2013-07-01 14:08:12 -0700

    Updated to beta7 of editing framework.

--- html/sites/all/themes/wl/templates/page.tpl.php
+++ html/sites/all/themes/wl/templates/page.tpl.php
@@ -34,11 +34,11 @@
             <?php if (empty($page['navigation'])): ?>
               <div class="nav-collapse">
                 <nav role="navigation">
-                <?php if ($primary_nav): ?>
-                  <?php print $primary_nav; ?>
+                <?php if (!empty($primary_nav)): ?>
+                  <?php print render($primary_nav); ?>
                 <?php endif; ?>
-                <?php if ($secondary_nav): ?>
-                  <?php print $secondary_nav; ?>
+                <?php if (!empty($secondary_nav)): ?>
+                  <?php print render($secondary_nav); ?>
                 <?php endif; ?>
                 </nav>
               </div>
